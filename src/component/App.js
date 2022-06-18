7af5ae98435bbebe1eeba9a81057c62b521e1d7a
Maina, Stephen <m41na@yahoo.com>
2022-06-18 12:18:44 -0500

    initial commit

---
+++ src/component/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import firebase from '../state/FirebaseConfig';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='title-row'>
+        <h2 className='title'>Firebase Marketplace</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
